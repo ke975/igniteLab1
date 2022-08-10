d8a1100c9ca1fae6fa8bb0deedbab9f8290f6a68
ke975 <kevcastle123@gmail.com>
2022-08-09 20:35:21 -0600

    Update apollo.ts

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri:'https://api-us-east-1.graphcms.com/v2/cl4yy5xi90ew801rqcdj797tk/master',
+    uri:'https://api-sa-east-1.hygraph.com/v2/cl6icweav4o8o01uq8vwdbbqn/master',
     cache:new InMemoryCache()
 })
